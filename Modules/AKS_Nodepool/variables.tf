2fa22aab3ac2725b12328893282692387c7a0912
OktaySavdi <oktay.savdi@gmail.com>
2024-02-12 12:38:35 +0100

    Create variables.tf

---
+++ Modules/AKS_Nodepool/variables.tf
@@ -0,0 +1,13 @@
+variable "node_pools" {}
+
+variable "resource_group_name" {
+  type = string
+}
+
+variable "location" {
+  type = string
+}
+
+variable "kubernetes_cluster_id" {
+  type = string
+}
