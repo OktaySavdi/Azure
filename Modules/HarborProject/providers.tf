089043e10c615b87c68bf6c16426e3a86db0e97b
OktaySavdi <oktay.savdi@gmail.com>
2024-03-01 13:32:25 +0100

    Update providers.tf

--- Modules/HarborProject/providers.tf
+++ Modules/HarborProject/providers.tf
@@ -1,8 +1,8 @@
 terraform {
   required_providers {
     harbor = {
-      source  = "BESTSELLER/harbor"
-      version = "3.7.1"
+      source  = "goharbor/harbor"
+      version = "3.10.8"
     }
   }
 }
@@ -12,13 +12,22 @@ provider "azurerm" {
   skip_provider_registration = "true"
 }
 
+provider "azurerm" {
+  alias           = "keyvault-sub"
+  subscription_id = "<sub_id>"
+  tenant_id       = "<tenant_id>"
+  features {}
+}
+
 ### Data sources ###
 data "azurerm_key_vault" "kv" {
+  provider            = azurerm.keyvault-sub
   name                = var.key_vault_name
   resource_group_name = var.resource_group_name
 }
 
 data "azurerm_key_vault_secret" "harbor_token" {
+  provider     = azurerm.keyvault-sub
   name         = var.key_vault_secret_name
   key_vault_id = data.azurerm_key_vault.kv.id
 }
