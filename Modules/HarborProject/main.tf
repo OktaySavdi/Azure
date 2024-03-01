a61ecc90012be360962d22ad6cdcf88bfc07c906
OktaySavdi <oktay.savdi@gmail.com>
2024-03-01 13:30:32 +0100

    Update main.tf

--- Modules/HarborProject/main.tf
+++ Modules/HarborProject/main.tf
@@ -26,16 +26,18 @@ resource "harbor_retention_policy" "main" {
   dynamic "rule" {
     for_each = var.image_retention_policy
     content {
-      n_days_since_last_pull = var.image_retention_policy[count.index].n_days_since_last_pull
-      tag_matching           = var.image_retention_policy[count.index].tag_matching
-      disabled               = var.image_retention_policy[count.index].disabled
-      always_retain          = var.image_retention_policy[count.index].always_retain
-      repo_matching          = var.image_retention_policy[count.index].repo_matching
-      repo_excluding         = var.image_retention_policy[count.index].repo_excluding
-      tag_excluding          = var.image_retention_policy[count.index].tag_excluding
-      untagged_artifacts     = var.image_retention_policy[count.index].untagged_artifacts
+      n_days_since_last_pull = try(rule.value.n_days_since_last_pull, null)
+      tag_matching           = try(rule.value.tag_matching, null)
+      disabled               = try(rule.value.disabled, null)
+      always_retain          = try(rule.value.always_retain, null)
+      repo_matching          = try(rule.value.repo_matching, null)
+      repo_excluding         = try(rule.value.repo_excluding, null)
+      tag_excluding          = try(rule.value.tag_excluding, null)
+      untagged_artifacts     = try(rule.value.untagged_artifacts, null)
+      n_days_since_last_push = try(rule.value.n_days_since_last_push, null)
+      most_recently_pulled   = try(rule.value.most_recently_pulled, null)
+      most_recently_pushed   = try(rule.value.most_recently_pushed, null)
     }
   }
-
   depends_on = [harbor_project.harborproject]
 }
