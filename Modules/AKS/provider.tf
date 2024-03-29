91127a196ee58945afa5fad7757ba28dec4b2825
OktaySavdi <oktay.savdi@gmail.com>
2024-03-29 13:30:56 +0100

    Update provider.tf

--- Modules/AKS/provider.tf
+++ Modules/AKS/provider.tf
@@ -4,11 +4,7 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = ">= 3.44.1"
-    }
-    tls = {
-      source  = "hashicorp/tls"
-      version = ">= 4.0.4"
+      version = "=3.97.1"
     }
   }
 }
