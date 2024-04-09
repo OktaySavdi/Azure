d0f74c56b461446f8305963498120a52ca135770
OktaySavdi <oktay.savdi@gmail.com>
2024-04-09 14:33:56 +0200

    Update provides.tf

--- Modules/PrivateEndpoint/provides.tf
+++ Modules/PrivateEndpoint/provides.tf
@@ -2,11 +2,11 @@ terraform {
   required_providers {
     azuread = {
       source  = "hashicorp/azuread"
-      version = "2.34.1"
+      version = "2.47.0"
     }
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "=3.44.1"
+      version = "=3.97.1"
     }
   }
   required_version = ">= 1.1.9"
