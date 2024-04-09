b8c19b3979df4e93ed7b9968b52485b0f24f0560
OktaySavdi <oktay.savdi@gmail.com>
2024-04-09 13:11:50 +0200

    Update outputs.tf

--- Modules/Keyvault/outputs.tf
+++ Modules/Keyvault/outputs.tf
@@ -1,14 +1,19 @@
 output "key_vault_id" {
-  description = "Id of the Key Vault"
-  value       = azurerm_key_vault.keyvault.id
+  description = "The ID of the Key Vault."
+  value       = azurerm_key_vault.main.id
 }
 
 output "key_vault_name" {
-  description = "Name of the Key Vault"
-  value       = azurerm_key_vault.keyvault.name
+  description = "Name of key vault created."
+  value       = azurerm_key_vault.main.name
 }
 
 output "key_vault_uri" {
-  description = "URI of the Key Vault"
-  value       = azurerm_key_vault.keyvault.vault_uri
+  description = "The URI of the Key Vault, used for performing operations on keys and secrets."
+  value       = azurerm_key_vault.main.vault_uri
+}
+
+output "key_vault_private_endpoint" {
+  description = "The ID of the Key Vault Private Endpoint"
+  value       = var.enable_private_endpoint ? element(concat(azurerm_private_endpoint.pep1.*.id, [""]), 0) : null
 }
