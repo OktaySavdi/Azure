3cbc159f55e7074cad4b26a65ca9bd99ead90b6a
OktaySavdi <oktay.savdi@gmail.com>
2024-04-30 16:33:23 +0200

    Update main.tf

--- Modules/PublicIP/main.tf
+++ Modules/PublicIP/main.tf
@@ -1,10 +1,10 @@
-resource "azurerm_public_ip" "example" {
+resource "azurerm_public_ip" "pi" {
   count               = length(var.public_ip)
   name                = var.public_ip[count.index].name
-  resource_group_name = var.resource_group_name
-  location            = var.location
+  resource_group_name = var.public_ip[count.index].resource_group_name
+  location            = var.public_ip[count.index].location
   allocation_method   = var.public_ip[count.index].allocation_method
   sku                 = var.public_ip[count.index].sku
-
-  tags = var.tags
+  domain_name_label   = var.public_ip[count.index].domain_name_label == "" ? null : var.public_ip[count.index].domain_name_label
+  tags                = var.public_ip[count.index].tags
 }
